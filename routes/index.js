693e6455789e16ccd990e251e7377c055fceb8e9
ImEtienne <graddykibongani@gmail.com>
2024-12-25 19:44:29 +0100

    :triangular_flag_on_post: reorganizing files and logic

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { getAllProjects } = require('../controllers/projectsController');
 
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Portfolio', description: 'Bienvenue sur mon site' });
+router.get('/', async (req, res) => {
+    try {
+        // la liste des projets sur la page d'accueil
+        const projects = await getAllProjects();
+        
+        res.render('index', {
+            portfolioItems: projects 
+        });
+    } catch (error) {
+        console.error('Erreur lors de la requête MySQL :', error);
+        res.status(500).send('Erreur serveur');
+    }
 });
 
 module.exports = router;
